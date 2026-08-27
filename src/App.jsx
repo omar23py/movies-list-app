69050deb358fdc68b78c111d0f04c4c431493cd5
Omar Yousry <152629913+omar23py@users.noreply.github.com>
2026-08-27 04:03:38 +0300

    feat: initialize app component and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import {useState , useEffect} from "react";
-
+import {useState, useEffect} from "react";
+import './index.css';
 
 export default function App() {
 
 
+    return (
+        <header>
+            <div className="header-container">
+                <div className='header-content-container'>
+                    <img class='w-auto' src='../public/logo.png' alt='logo' />
+                    <img src='../public/hero-image.png' alt='hero-image'/>
+                    <h1>Find <span class='text-gradient'>Movies</span> You'll Love Without The Hassle</h1>
+                </div>
+            </div>
+        </header>
 
+
+    )
 
 }
