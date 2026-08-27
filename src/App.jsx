ed1bfe24818ef9bdc62cde24d1d41932674a9dab
Omar Yousry <152629913+omar23py@users.noreply.github.com>
2026-08-28 01:59:26 +0300

    feat: in the app added pattern div and wrapper  div and mapped moveList into Card.                           make styling better for the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,86 +3,75 @@ import Search from "../components/Search.jsx";
 import Card from "../components/Card.jsx";
 import './index.css';
 
-
-
 export default function App() {
     const [searchTerm, setSearchTerm] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [moveList, setMoveList] = useState([]);
 
     const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-    const Base_URL='https://api.themoviedb.org/3';
+    const Base_URL = 'https://api.themoviedb.org/3';
     const API_OPTIONS = {
         'method': 'GET',
-        headers: {accept: 'application/json',
+        headers: {
+            accept: 'application/json',
             Authorization: `Bearer ${API_KEY}`,
         },
     };
 
-
-
-
-        async  function fetchMovies(query='') {
+    async function fetchMovies(query = '') {
         try {
-            console.log(API_KEY);
-            const endpoint = query?
-                `${Base_URL}/search/movie?query=${encodeURIComponent(query)}`:
-                `${Base_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query
+                ? `${Base_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${Base_URL}/discover/movie?sort_by=popularity.desc`;
 
-
-            const response = await fetch(endpoint,API_OPTIONS);
+            const response = await fetch(endpoint, API_OPTIONS);
 
             if (!response.ok) {
                 throw new Error('Error fetching movies.');
             }
             const data = await response.json();
 
-            if (data.Response =='Fales') {
-                setErrorMessage(data.Error || 'Failed to fetch movies');
+            if (data.results.length === 0) {
+                setErrorMessage(data.Error || 'No movies found.');
                 setMoveList([]);
                 return;
             }
-            console.log(data);
-
-
+            setMoveList(data.results);
+        } catch (error) {
+            console.log(`Error fetching movies ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later.');
+        } finally {
         }
-        catch (error) {
-            console.log(error);
-
-
-        }
-        finally {
-
-        }
-
-
-
-
     }
 
     useEffect(() => {
         fetchMovies(searchTerm);
     }, [searchTerm]);
+
     return (
-        <>
-            <header>
-                <div className="header-container">
-                    <div className='header-content-container'>
-                        <img class='w-auto' src='../public/logo.png' alt='logo'/>
-                        <img src='../public/hero-image.png' alt='hero-image'/>
-                        <h1>Find <span class='text-gradient'>Movies</span> You'll Love Without The Hassle</h1>
-                        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <main>
+            <div className="pattern" />
+            <div className="wrapper">
+                <header>
+                    <div className="header-container">
+                        <div className='header-content-container'>
+                            <img className='w-auto' src='../public/logo.png' alt='logo' />
+                            <img src='/hero-image.png' alt='hero-image' />
+                            <h1>Find <span className='text-gradient'>Movies</span> You'll Love Without The Hassle</h1>
+                            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                        </div>
                     </div>
-                </div>
-            </header>
-            <section className='all-movies'>
-                <Card/>
-            </section>
-
-
-        </>
-
-
-    )
-
+                </header>
+                <section className='Popular-movies'>
+                    <ul>
+                        {moveList.map(movie => (
+                            <li key={movie.id}>
+                                <Card movie={movie} />
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </div>
+        </main>
+    );
 }
