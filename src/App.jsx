83d26f0384ec73372b569678482bbe58b2a1b4a3
Omar Yousry <152629913+omar23py@users.noreply.github.com>
2026-08-27 06:17:31 +0300

    fect: added useState hook for the search term & useEffect Hook for future use.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import {useState, useEffect} from "react";
+import Search from "../components/Search";
 import './index.css';
 
 export default function App() {
+const [searchTerm, setSearchTerm] = useState('');
 
-
+useEffect(() => {
+},[searchTerm]);
     return (
         <header>
             <div className="header-container">
@@ -11,6 +14,7 @@ export default function App() {
                     <img class='w-auto' src='../public/logo.png' alt='logo' />
                     <img src='../public/hero-image.png' alt='hero-image'/>
                     <h1>Find <span class='text-gradient'>Movies</span> You'll Love Without The Hassle</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </div>
             </div>
         </header>
